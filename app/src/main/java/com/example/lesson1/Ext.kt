6100d37ec9c3e960e9939b2eff74d7c7b10e1d2e
KurbanovAziz <ripplemobile29@gmail.com>
2023-02-23 19:10:51 +0600

    registerForResult

--- app/src/main/java/com/example/lesson1/Ext.kt
+++ app/src/main/java/com/example/lesson1/Ext.kt
@@ -3,6 +3,6 @@ package com.example.lesson1
 import android.content.Context
 import android.widget.Toast
 
-fun makeText(context: Context, setText: String) {
-    Toast.makeText(context, setText, Toast.LENGTH_SHORT).show()
+fun Context.makeText(setText: String) {
+    Toast.makeText(this, setText, Toast.LENGTH_SHORT).show()
 }
